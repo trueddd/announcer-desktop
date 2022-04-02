1158efacf0f9bfbbdff0d2013d25457724c46269
trueddd <dmitry.kuzmich@orangesoft.co>
2022-04-02 19:00:00 +0300

    ci fix [3]

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,14 @@ tasks.named<Test>("test") {
     useJUnitPlatform()
 }
 
+tasks.register("createComposePackage") {
+    dependsOn("packageMsi")
+    doLast {
+        val file = File("${buildDir.absolutePath}/compose/binaries/main/msi")
+        println("Built files: ${file.listFiles()?.joinToString()}")
+    }
+}
+
 compose.desktop {
     application {
         mainClass = "MainKt"
