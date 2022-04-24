f9fce9a9b5775080001220d4916f01dcd6f09474
trueddd <dkuzmichn1ce@gmail.com>
2022-04-24 20:25:34 +0300

    debug actions fix [6]: crash logs saving

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,31 @@
 import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowExceptionHandler
 import androidx.compose.ui.window.application
+import com.soywiz.klock.DateFormat
+import com.soywiz.klock.DateTime
 import di.AppStopper
 import di.appModule
 import navigation.MainNavigator
+import java.io.File
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main(vararg args: String) = application {
     val appParams = getApplicationParameter(args)
     Window(
         onCloseRequest = ::exitApplication,
         title = "Announcer",
     ) {
+        LaunchedEffect(Unit) {
+            window.exceptionHandler = WindowExceptionHandler {
+                val crashLogFile = File("${System.getenv("APPDATA")}/announcer/logs", "crash-${DateTime.now().format(DateFormat.FORMAT1)}.txt")
+                crashLogFile.createNewFile()
+                crashLogFile.writeText(it.stackTraceToString())
+            }
+        }
         println("App launched with parameters: $appParams")
         appModule.single<AppStopper> { ::exitApplication }
         val navigator = MainNavigator(appParams)
